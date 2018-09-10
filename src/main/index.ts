ca11470294fa535a54d97c9f1c57bd3bfdc6859c
uittorio <vittorio.gue@gmail.com>
2018-09-10 17:48:27 +0100

    get quotes from api instead locally

--- src/main/index.ts
+++ src/main/index.ts
@@ -10,8 +10,9 @@ import { NotificationService } from "./notificationService/notificationService";
 import { OnceADay } from "./frequency/onceADay";
 import { QuoteNotification } from "./quoteNotification/quoteNotification";
 import { StandardRandomizer } from "./randomizer/standardRandomizer";
-import { QuotesData } from "../quotes/quotes";
 const Store = require('electron-store');
+const https = require('https');
+const quoteUrl: string = "https://api.jsonbin.io/b/5b9584323ffac56f4bdbf2ad/latest";
 
 QuoteApp.create().then(() => {
 	let windowLoader: WindowLoader;
@@ -35,14 +36,28 @@ QuoteApp.create().then(() => {
 	});
 	
 	window.ready().then(() => {
+		https.get(quoteUrl, (resp: any) => {
+			let data = "";
+			
+			resp.on('data', (chunk: any) => {
+				data += chunk;
+			});
+			
+			resp.on('end', () => {
+				showQuote(JSON.parse(data).quotes);
+			});
+		});
+	});
+	
+	function showQuote(data: Array<QuoteData>) {
 		const timeInFrequency = QuotesDate.hourMinutes(16, 0);
 		
-		const notificationService = new NotificationService<QuoteData>(QuotesData, new OnceADay(), timeInFrequency, new Store(), randomizer);
+		const notificationService = new NotificationService<QuoteData>(data, new OnceADay(), timeInFrequency, new Store(), randomizer);
 		
 		QuoteNotification.get(notificationService, QuotesDate.now(), window, notificationEnabled);
 		
 		setInterval(() => {
 			QuoteNotification.get(notificationService, QuotesDate.now(), window, notificationEnabled);
 		}, 3000);
-	});
+	}
 });
